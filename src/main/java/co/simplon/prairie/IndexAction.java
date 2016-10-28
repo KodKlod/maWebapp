5cf56a52daedddf428705a9ab92174e81d259bf6
KodKlod <codingklod@gmail.com>
2016-10-28 11:55:30 +0200

    Affichage team

--- src/main/java/co/simplon/prairie/IndexAction.java
+++ src/main/java/co/simplon/prairie/IndexAction.java
@@ -6,15 +6,41 @@ import jodd.madvoc.meta.Out;
 
 import java.util.List;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import oracle.jdbc.OracleDriver;
+
+
 @MadvocAction
 public class IndexAction {
+static String databaseUrl = "jdbc:oracle:thin:SIMPLON/SIMPLON@localhost:1521:XE";
+
+static String requeteSql = "select NAME from TEAMS where CITY='San Francisco'";
 
     @Out
     String prenom;
+    @Out
+    String nom;
 
     @Action("/")
-    public String view() {
-        prenom = "Eric";
-        return "/index";
+    public String view () throws Exception{
+        prenom = "Claude";
+     
+		DriverManager.registerDriver(new OracleDriver());
+
+        Connection connexion = DriverManager.getConnection(databaseUrl);
+        Statement requete = connexion.createStatement();
+        ResultSet resultat = requete.executeQuery(requeteSql);
+        while (resultat.next()) {
+            nom = resultat.getString("NAME");
+            System.out.println(nom);
+        }
+        resultat.close();
+        requete.close();
+        connexion.close();
+    	return "/index";   
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,6 +3,18 @@
 <h1>Mon application Web en Java</h1>
 
 Ceci est une page statique.
+<!-- affichage du prénom en appelant @MadvocAction framework JODD -->
+<p> J'affiche mon prénom ${prenom}</p> <!-- syntaxe EL (Expression language) -->
 
+
+<!-- Comment afficher l'équipe de San Fransisco -->
+<!-- select NAME from TEAMS
+	where CITY='San Francisco';
+
+	select FNAME, LNAME from PLAYERS, TEAMS
+	where CITY='San Francisco'
+	order by LNAME;
+ -->
+<p> Et là j'affiche le nom de l'équipe de San Francisco  ${nom}</p>
 </body>
 </html>
